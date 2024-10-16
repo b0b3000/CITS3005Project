774bb375e4849957518291cfc85225595d386d02
Martin-EvansUWA <23621647@student.uwa.edu.au>
2024-10-16 19:04:32 +0800

    add ancestor search, with sub words

--- flaskapp/searches.py
+++ flaskapp/searches.py
@@ -16,14 +16,15 @@ def run_search(search_type: str, search_input: str,  graph: Graph):
 def get_ancestors(item_name: str, mac: Ontology):
     if item_name == "":
         return []
+    sub_words = item_name.split(" ")
     possible_ancestors = []
     # get all items
     items = list(mac.Item.instances())
     print(type(items))
     print(items)
-    for item in items:
-        current_item_name = str(item).replace("mac.", "")
-        print(current_item_name)
-        if item_name.lower() in current_item_name.lower():
-            possible_ancestors.append(current_item_name)
+    for sub_word in sub_words:
+        for item in items:
+            current_item_name = str(item).replace("mac.", "")
+            if sub_word.lower() in current_item_name.lower():
+                possible_ancestors.append(current_item_name)
     return possible_ancestors
